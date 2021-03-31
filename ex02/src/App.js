492cb49ab853c2b25d4c244abde70b169e291289
hajdukka <nerminhajdo@gmail.com>
2021-03-31 06:15:49 +0200

    ex02&ex03&struktura

---
+++ ex02/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+import Plant from "./Plant";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Plant />
+      </div>
+    );
+  }
+}
+
+export default App;
